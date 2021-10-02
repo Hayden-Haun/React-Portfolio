dae3f2eb52188d6e40f8fc0453df2906e5aaa53e
Hayden-Haun <haydenhaun@gmail.com>
2021-10-02 00:05:41 -0600

    validate email working in Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
-import { FormControl, FormGroup, Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import validator from "validator";
 
 export default function Contact() {
-  const [emailError, setEmailError] = useState("");
-  const validateEmail = (e) => {
-    var email = e.target.value;
+  const [email, setEmail] = useState("");
 
-    if (validator.isEmail(email)) {
-      setEmailError("Valid Email :)");
-    } else {
-      setEmailError("Enter valid Email!");
+  const [message, setMessage] = useState("");
+
+  // const [hoveredText, setHoveredText] = useState(false);
+
+  function checkValidEmail(e) {
+    if (!validator.isEmail(email)) {
+      alert("Invalid email");
+    }
+  }
+
+  const checkText = () => {
+    if (hoveredText) {
+      setHoveredText(false);
+      // alert("Please provide a message");
     }
   };
+
+  // const emailState = (e = {});
+
+  // const validateEmail = (e) => {
+  //   var email = e.target.value;
+
+  //   if (validator.isEmail(email)) {
+  //     setEmail("Valid Email :)");
+  //   } else {
+  //     setEmail("Enter valid Email!");
+  //   }
+  // };
 
   return (
     <div className="d-flex justify-content-center align-items-center">
@@ -38,16 +58,12 @@ export default function Contact() {
         id="links"
       >
         <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-          <h1>
-            <strong>Contact me!</strong>
-          </h1>
-
           <h2 className="mt-3">
             <a href="mailto:haydenhaun@gmail.com" className="text-purp">
               303-718-1311
             </a>
           </h2>
-          <h2 className="">
+          <h2 className="mt-1">
             <a href="mailto:haydenhaun@gmail.com" className="text-purp">
               haydenhaun@gmail.com
             </a>
@@ -57,28 +73,51 @@ export default function Contact() {
         <Form className="mt-5">
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>
-              <h5>
-                <strong>Email:</strong>
+              <h5 className="text-purp">
+                <strong>Your Email:</strong>
               </h5>
             </Form.Label>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <div class="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+                onChange={(event) => setEmail(event.target.value)}
+                // onSubmit={() => checkValidEmail()}
+              />
+            </div>
+
+            {/* <Form.Control
+              type="email"
+              onChange={(event) => setEmail(event.target.value)}
+            /> */}
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>
-              <h5>
-                <strong>Message</strong>
+              <h5 className="text-purp">
+                <strong>Message:</strong>
               </h5>
             </Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                rows="3"
+                onChange={(event) => setMessage(event.target.value)}
+                // onMouseEnter={setHoveredText(true)}
+                // onMouseLeave={setHoveredText(false)}
+              ></textarea>
+            </div>
           </Form.Group>
         </Form>
+        <Button
+          variant="success"
+          className="text-purp"
+          style={{ width: "15vw" }}
+          onClick={(e) => checkValidEmail(e)}
+        >
+          Submit
+        </Button>
       </div>
     </div>
   );
